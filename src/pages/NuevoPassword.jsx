1b2e343d7aaedfce76a6bc7e51668aa90baae5f8
juanesgv <juanesgarviedma23@gmail.com>
2024-02-15 13:42:24 -0500

    Formulario para reestablecer contraseña (envío de intrucciones y establecer nueva contraseña)

--- src/pages/NuevoPassword.jsx
+++ src/pages/NuevoPassword.jsx
@@ -1,27 +1,102 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Link, useParams, useNavigate } from "react-router-dom"
+import axios from "axios"
+import Alerta from "../components/Alerta"
 
 const NuevoPassword = () => {
+
+    const [tokenValido, setTokenValido] = useState(false)
+    const [passwordModificado, setPasswordModificado] = useState(false)
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const navigate = useNavigate();
+    const params = useParams()
+    const { token } = params
+
+    useEffect(() => {
+        const comprobarToken = async () => {
+            try {
+                await axios(`${import.meta.env.VITE_API_URL}/api/usuarios/olvide-password/${token}`)
+                setTokenValido(true)
+            } catch (error) {
+                console.log(error.response)
+                setAlerta({
+                    msg: error.response.data.msg,
+                    error: true
+                })
+            }
+        }
+        comprobarToken()
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if(password.length < 6 ){
+            setAlerta({
+                msg : "La contraseña debe tener al menos 6 caracteres",
+                error : true
+            })
+            return
+        }
+
+        try {
+            const url = `${import.meta.env.VITE_API_URL}/api/usuarios/olvide-password/${token}`
+            const {data} = await axios.post(url,{password})
+            setAlerta({
+                msg: data.msg,
+                error: false
+            })
+            setPasswordModificado(true)
+            setTimeout(() => {
+                navigate('/');
+              }, 5000);
+            
+        } catch (error) {
+            console.log(error)
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+
+    const {msg} = alerta
+
     return (
         <>
             <h1 className='text-sky-600 font-black text-6xl'>Reestablece tu contraseña  y  continua administrando tus <span className='text-slate-700'> proyectos </span> </h1>
 
-            <form className='my-10 bg-white shadow rounded-lg p-8'>
-                <div className='my-5'>
-                    <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='password'>Nueva contraseña</label>
-                    <input
-                        type='password'
-                        id='password'
-                        placeholder='Escribe tu nueva oontraseña'
-                        className='w-full mt-3 p-3 border rounded bg-gray-50'
-                    />
-                </div>
+            {msg && <Alerta alerta={alerta}/>}
 
-                <input
-                    type='submit'
-                    value={"Reestablecer contraseña"}
-                    className='bg-sky-700 w-full py-3 mb-8 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
-                />
-            </form>
+            {tokenValido && (
+                <form className='my-10 bg-white shadow rounded-lg p-8' onSubmit={handleSubmit}>
+                    <div className='my-5'>
+                        <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='password'>Nueva contraseña</label>
+                        <input
+                            type='password'
+                            id='password'
+                            placeholder='Escribe tu nueva oontraseña'
+                            className='w-full mt-3 p-3 border rounded bg-gray-50'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <input
+                        type='submit'
+                        value={"Reestablecer contraseña"}
+                        className='bg-sky-700 w-full py-3 mb-8 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
+                    />
+                </form>
+            )}
+
+            {passwordModificado && (
+                <Link to="/" className="block text-center mb-5 text-slate-500 uppercase text-sm hover:text-sky-700 hover:font-medium">
+                    Inicia sesión
+                </Link>
+            )}
         </>
     )
 }
--- src/pages/OlvidePassword.jsx
+++ src/pages/OlvidePassword.jsx
@@ -1,11 +1,48 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import Alerta from "../components/Alerta"
+import axios from "axios"
 
 const OlvidePassword = () => {
+
+    const [email, setEmail] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if(email === '' || email.length < 6){
+            setAlerta({
+                msg: "El email es obligatorio",
+                error : true
+            })
+            return
+        }
+
+        try {
+            const api_url = import.meta.env.VITE_API_URL
+            const {data} = await axios.post(`${api_url}/api/usuarios/olvide-password`,{email})
+            setAlerta({
+                msg: data.msg,
+                error:false
+            })
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+
+    const {msg} = alerta
+
     return (
         <>
             <h1 className='text-sky-600 font-black text-6xl'>Recupera tu contraseña y no pierdas tus <span className='text-slate-700'> proyectos </span> </h1>
 
-            <form className='my-10 bg-white shadow rounded-lg p-8'>
+            {msg && <Alerta alerta={alerta} />}
+
+            <form className='my-10 bg-white shadow rounded-lg p-8' onSubmit={handleSubmit}>
                 <div className='my-5'>
                     <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='email'>Email</label>
                     <input
@@ -13,6 +50,8 @@ const OlvidePassword = () => {
                         id='email'
                         placeholder='Email de registro'
                         className='w-full mt-3 p-3 border rounded bg-gray-50'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
 
