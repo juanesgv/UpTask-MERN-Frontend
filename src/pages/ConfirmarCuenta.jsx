750fd35278a980c8ea6f0ee6501ebd99819f80bf
juanesgv <juanesgarviedma23@gmail.com>
2024-02-15 00:02:48 -0500

    Implementación de la página de confirmar cuenta

--- src/pages/ConfirmarCuenta.jsx
+++ src/pages/ConfirmarCuenta.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import axios from "axios"
+import Alerta from "../components/Alerta"
 
 const ConfirmarCuenta = () => {
+
+    const [alerta, setAlerta] = useState({})
+    const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+
+    const params = useParams()
+    const { token } = params
+
+    useEffect(() => {
+        const confirmarCuenta = async () => {
+            try {
+
+                const url = `${import.meta.env.VITE_API_URL}/api/usuarios/confirmar/${token}`
+                const { data } = await axios.get(url)
+                setAlerta({
+                    msg: data.msg,
+                    error: false
+                })
+                setCuentaConfirmada(true)
+
+            } catch (error) {
+                setAlerta({
+                    msg: error.response.data.msg,
+                    error: true
+                })
+            }
+        }
+        confirmarCuenta()
+    }, [])
+
+    const { msg } = alerta
+
     return (
         <>
-            <h1 className='text-sky-600 font-black text-6xl'>Confirma tu cuenta  y  administra <span className='text-slate-700'> proyectos </span> </h1>
+            <h1 className='text-sky-600 font-black text-6xl mb-20'>Confirma tu cuenta  y  administra <span className='text-slate-700'> proyectos </span> </h1>
+
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-lg bg-white">
+                {msg && <Alerta alerta={alerta} />}
+
+
+                <Link to="/" className="block text-center mb-5 text-slate-500 uppercase text-sm hover:text-sky-700 hover:font-medium">
+                    Inicia sesión
+                </Link>
+
+            </div>
         </>
     )
 }
